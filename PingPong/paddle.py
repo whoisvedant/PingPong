c88b47197d64dc12d722dbb5b552be49f52092f3
Vedant Jadhav <136951168+whoisvedant@users.noreply.github.com>
2024-06-25 08:44:34 +0530

    added PingPong

---
+++ PingPong/paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.paddle = Turtle()
+        self.paddle.shape("square")
+        self.paddle.color("White")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.penup()
+        self.paddle.goto(position)
+
+    def go_up(self):
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
